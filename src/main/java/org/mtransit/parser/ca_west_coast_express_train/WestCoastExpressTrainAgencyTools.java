994ff1efdad90540eb8a5042a30398e389eef7d8
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-01 20:54:30 -0500

    GTFS Direction Finder > enabled

--- src/main/java/org/mtransit/parser/ca_west_coast_express_train/WestCoastExpressTrainAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_west_coast_express_train/WestCoastExpressTrainAgencyTools.java
@@ -1,13 +1,11 @@
 package org.mtransit.parser.ca_west_coast_express_train;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
+import org.mtransit.parser.StringUtils;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
@@ -17,8 +15,15 @@ import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.mt.data.MTrip;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+import static org.mtransit.parser.StringUtils.EMPTY;
 
 // http://www.translink.ca/en/Schedules-and-Maps/Developer-Resources.aspx
 // http://www.translink.ca/en/Schedules-and-Maps/Developer-Resources/GTFS-Data.aspx
@@ -28,7 +33,7 @@ import org.mtransit.parser.mt.data.MTrip;
 // http://gtfs.translink.ca/static/latest
 public class WestCoastExpressTrainAgencyTools extends DefaultAgencyTools {
 
-	public static void main(String[] args) {
+	public static void main(@Nullable String[] args) {
 		if (args == null || args.length == 0) {
 			args = new String[3];
 			args[0] = "input/gtfs.zip";
@@ -38,96 +43,98 @@ public class WestCoastExpressTrainAgencyTools extends DefaultAgencyTools {
 		new WestCoastExpressTrainAgencyTools().start(args);
 	}
 
-	private HashSet<String> serviceIds;
+	@Nullable
+	private HashSet<Integer> serviceIdInts;
 
 	@Override
-	public void start(String[] args) {
-		System.out.printf("\nGenerating West Coast Express train data...");
+	public void start(@NotNull String[] args) {
+		MTLog.log("Generating West Coast Express train data...");
 		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this);
+		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
 		super.start(args);
-		System.out.printf("\nGenerating West Coast Express train data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+		MTLog.log("Generating West Coast Express train data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
 	@Override
 	public boolean excludingAll() {
-		return this.serviceIds != null && this.serviceIds.isEmpty();
+		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
 	}
 
 	@Override
-	public boolean excludeCalendar(GCalendar gCalendar) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendar(gCalendar, this.serviceIds);
+	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
+		if (this.serviceIdInts != null) {
+			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
 		}
 		return super.excludeCalendar(gCalendar);
 	}
 
 	@Override
-	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
+	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
+		if (this.serviceIdInts != null) {
+			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
 		}
 		return super.excludeCalendarDate(gCalendarDates);
 	}
 
 	@Override
-	public boolean excludeRoute(GRoute gRoute) {
+	public boolean excludeRoute(@NotNull GRoute gRoute) {
+		//noinspection RedundantIfStatement
 		if (!RSN_WCE.contains(gRoute.getRouteShortName())) {
 			return true; // exclude
 		}
 		return false; // keep
 	}
 
-	private static final String TRAINBUS_THS_LC = "trainbus";
+	private static final String TRAIN_BUS_THS_LC = "trainbus";
 
 	@Override
-	public boolean excludeTrip(GTrip gTrip) {
-		if (gTrip.getTripHeadsign().toLowerCase(Locale.ENGLISH).contains(TRAINBUS_THS_LC)) {
+	public boolean excludeTrip(@NotNull GTrip gTrip) {
+		final String tripHeadSignLC = gTrip.getTripHeadsignOrDefault().toLowerCase(Locale.ENGLISH);
+		if (tripHeadSignLC.contains(TRAIN_BUS_THS_LC)) {
 			return true; // TrainBus is a bus, not a train
 		}
-		if (this.serviceIds != null) {
-			return excludeUselessTrip(gTrip, this.serviceIds);
+		if (this.serviceIdInts != null) {
+			return excludeUselessTripInt(gTrip, this.serviceIdInts);
 		}
 		return super.excludeTrip(gTrip);
 	}
 
+	@NotNull
 	@Override
 	public Integer getAgencyRouteType() {
 		return MAgency.ROUTE_TYPE_TRAIN;
 	}
 
 	@Override
-	public long getRouteId(GRoute gRoute) {
+	public long getRouteId(@NotNull GRoute gRoute) {
 		if (Utils.isDigitsOnly(gRoute.getRouteShortName())) {
 			return Long.parseLong(gRoute.getRouteShortName()); // use route short name as route ID
 		}
 		if (RSN_WCE.contains(gRoute.getRouteShortName())) {
 			return RID_WCE;
 		}
-		System.out.println("Unexpected route short name " + gRoute);
-		System.exit(-1);
-		return -1l;
+		throw new MTLog.Fatal("Unexpected route short name for %s!", gRoute);
 	}
 
-	private static final List<String> RSN_WCE = Arrays.asList(new String[] { "997", "WCE", "WEST COAST EXPRESS" });
+	private static final List<String> RSN_WCE = Arrays.asList("997", "WCE", "WEST COAST EXPRESS");
 	private static final String WCE_SHORT_NAME = "WCE";
 
+	@Nullable
 	@Override
-	public String getRouteShortName(GRoute gRoute) {
+	public String getRouteShortName(@NotNull GRoute gRoute) {
 		if (RSN_WCE.contains(gRoute.getRouteShortName())) {
 			return WCE_SHORT_NAME;
 		}
-		System.out.println("Unexpected route short name " + gRoute);
-		System.exit(-1);
-		return null;
+		throw new MTLog.Fatal("Unexpected route short name for %s!", gRoute);
 	}
 
-	private static final Pattern WCE_ = Pattern.compile("((^|\\W){1}(west coast express)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern WCE_ = Pattern.compile("((^|\\W)(west coast express)(\\W|$))", Pattern.CASE_INSENSITIVE);
 
+	@NotNull
 	@Override
-	public String getRouteLongName(GRoute gRoute) {
+	public String getRouteLongName(@NotNull GRoute gRoute) {
 		String routeLongName = gRoute.getRouteLongName();
-		routeLongName = WCE_.matcher(routeLongName).replaceAll(StringUtils.EMPTY); // removing trade-mark
+		routeLongName = WCE_.matcher(routeLongName).replaceAll(EMPTY); // removing trade-mark
 		return routeLongName;
 	}
 
@@ -135,42 +142,45 @@ public class WestCoastExpressTrainAgencyTools extends DefaultAgencyTools {
 
 	private static final String AGENCY_COLOR = AGENCY_COLOR_VIOLET;
 
+	@NotNull
 	@Override
 	public String getAgencyColor() {
 		return AGENCY_COLOR;
 	}
 
+	@Nullable
 	@Override
-	public String getRouteColor(GRoute gRoute) {
+	public String getRouteColor(@NotNull GRoute gRoute) {
 		return null; // use agency color
 	}
 
-	private static final long RID_WCE = 997l;
+	private static final long RID_WCE = 997L;
 
 	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		mTrip.setHeadsignString(cleanTripHeadsign(gTrip.getTripHeadsign()), gTrip.getDirectionId());
+	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
+		mTrip.setHeadsignString(
+				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
+				gTrip.getDirectionIdOrDefault()
+		);
 	}
 
 	@Override
-	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
-		System.out.printf("\n%s: Unexpected trips to merge: %s and %s!\n", mTrip.getRouteId(), mTrip, mTripToMerge);
-		System.exit(-1);
-		return false;
+	public boolean directionFinderEnabled() {
+		return true;
 	}
 
-	private static final Pattern WCE_TRAIN_TO = Pattern.compile("(^west coast express train to)", Pattern.CASE_INSENSITIVE);
-
 	@Override
-	public String cleanTripHeadsign(String tripHeadsign) {
-		if (Utils.isUppercaseOnly(tripHeadsign, true, true)) {
-			tripHeadsign = tripHeadsign.toLowerCase(Locale.ENGLISH);
-		}
-		tripHeadsign = WCE_TRAIN_TO.matcher(tripHeadsign).replaceAll(StringUtils.EMPTY);
+	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
+		throw new MTLog.Fatal("%s: Unexpected trips to merge: %s and %s!", mTrip.getRouteId(), mTrip, mTripToMerge);
+	}
+
+	@NotNull
+	@Override
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, tripHeadsign);
+		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
-
-	private static final Pattern ENDS_WITH_BOUND = Pattern.compile("((east|west|north|south)(bound)?[\\s]*$)", Pattern.CASE_INSENSITIVE);
 
 	private static final Pattern STATION_STN = Pattern.compile("(station|stn)", Pattern.CASE_INSENSITIVE);
 
@@ -178,24 +188,24 @@ public class WestCoastExpressTrainAgencyTools extends DefaultAgencyTools {
 
 	private static final String WCE_REPLACEMENT = "$2" + WCE_SHORT_NAME + "$4";
 
+	@NotNull
 	@Override
-	public String cleanStopName(String gStopName) {
-		if (Utils.isUppercaseOnly(gStopName, true, true)) {
-			gStopName = gStopName.toLowerCase(Locale.ENGLISH);
-		}
-		gStopName = STATION_STN.matcher(gStopName).replaceAll(StringUtils.EMPTY);
-		gStopName = UNLOADING.matcher(gStopName).replaceAll(StringUtils.EMPTY);
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, gStopName);
+		gStopName = STATION_STN.matcher(gStopName).replaceAll(EMPTY);
+		gStopName = UNLOADING.matcher(gStopName).replaceAll(EMPTY);
 		gStopName = WCE_.matcher(gStopName).replaceAll(WCE_REPLACEMENT);
-		gStopName = ENDS_WITH_BOUND.matcher(gStopName).replaceAll(StringUtils.EMPTY);
+		gStopName = CleanUtils.cleanBounds(gStopName);
 		gStopName = CleanUtils.cleanStreetTypes(gStopName);
 		return CleanUtils.cleanLabel(gStopName);
 	}
 
 	@Override
-	public int getStopId(GStop gStop) {
+	public int getStopId(@NotNull GStop gStop) {
 		if (!StringUtils.isEmpty(gStop.getStopCode()) && Utils.isDigitsOnly(gStop.getStopCode())) {
 			return Integer.parseInt(gStop.getStopCode()); // using stop code as stop ID
 		}
-		return 1000000 + Integer.parseInt(gStop.getStopId());
+		//noinspection deprecation
+		return 1_000_000 + Integer.parseInt(gStop.getStopId());
 	}
 }
